9a105bcc82bf99fd1785279049b500e63be358d9
Lillith Rose <imlvnaa@gmail.com>
2025-03-21 12:55:23 -0400

    --help argument and readme edit

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Allows you to control a pishock, either vibrate or shock, by buttplugio/intiface
 
 This program has a simple console interface, where help gives you a list of commands to edit your preferences
 
-no exe yet as serialport is annoying, just run from source
+## Downloads
+
+Downloads can either be found [Here](https://nightly.link/lillithkt/pishock-buttplugio/workflows/build/main/PiShock-ButtplugIO.exe.zip) if you are not logged in to github, or [here](https://github.com/lillithkt/pishock-buttplugio/actions/workflows/build.yml) if you are
 
 ## Warnings
 
--- src/debugcommands.ts
+++ src/debugcommands.ts
@@ -10,11 +10,35 @@ export const flags = {
 };
 
 export default async function runDebugCommands() {
+  if (process.argv.includes("-h") || process.argv.includes("--help"))
+    sendHelpCommand();
   if (process.argv.includes("--serial")) await serialDebug(true);
   if (process.argv.includes("--seriallogs")) await serialDebug(false);
   if (process.argv.includes("--vibrate")) await vibrateDebug();
   if (process.argv.includes("--shock")) await shockDebug();
   if (process.argv.includes("--debug")) flags.debugLogs = true;
+}
+
+function sendHelpCommand() {
+  console.log(
+    "Normally, this program is run without any arguments. However, there are certian ones for debug usage."
+  );
+  console.log("--help or -h shows this message!");
+  console.log(
+    "--serial opens a serial console where you can interface with the pishock hub"
+  );
+  console.log("--seriallogs just outputs the hub's logs to the console");
+  console.log(
+    "--vibrate sends a vibrate at 100 for 1 second to test your connection"
+  );
+  console.log(
+    "--shock shocks you at 10 for 0.3 seconds to test your connection"
+  );
+  console.log("--debug outputs extra debug logs");
+  console.log(
+    "Your config file is stored at AppData/Roaming/Pishock-Buttplugio/config.json if you would like to edit it or check all the data i store."
+  );
+  process.exit(0);
 }
 
 function serialDebug(shouldEnterReadline: boolean = true) {
